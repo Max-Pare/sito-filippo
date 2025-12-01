e6366312f33df2f4023d1f824e270a4877cde581
Max-Pare <resilientcrab@proton.me>
2025-12-01 14:45:28 +0100

    commit

--- server/server.py
+++ server/server.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder='site_files',      # directory on disk
+    static_url_path='/site_files'    # URL prefix
+)
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port='38104', debug=True)
