8a914a33e408f3da1e8a86fe600b83b1065243e7
Max-Pare <resilientcrab@proton.me>
2025-12-04 15:26:24 +0100

    started work on form sanitization

--- server/server.py
+++ server/server.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 
+form_char_limit_default = 96
+form_char_limit_notes = 1024
+
 app = Flask(
     __name__,
     static_folder='site_files',
@@ -13,16 +16,28 @@ def home():
 @app.route('/prenota', methods=['POST'])
 def prenota():
     data = request.form
+    
+    def check_too_long(obj:dict):
+        char_limit = form_char_limit_default
+        for key, value in obj.items():
+            if key is 'patientNotes':
+                char_limit = form_char_limit_notes
+            if len(value) > char_limit:
+                return True
+    
     data_obj = {
         'name'         : data['patientName'],
         'visitType'    : data['visitType'],
         'email'        : data['patientEmail'],
         'patientPhone' : data['patientPhone'],
         'patientNotes' : data['patientNotes']
-        
     }
-    print(data_obj)
-    return data_obj
+    del data
+    if check_too_long(data_obj): return "Uno dei dati inseriti e' troppo lungo, perfavore usa meno caratteri."
+    if len(data_obj.get(patientPhone)) > 13 or any(char not in '+1234567890' for char in data_obj.get(patientPhone)):
+        return "Numero di telefono non valido."
+    
+    return f"<h1>Richiesta ricevuta, verrete contattati al piu' presto.</h1><div></div><p>{data_obj}</p>"
 
 
 if __name__ == "__main__":
