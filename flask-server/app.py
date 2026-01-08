dc08f23b3dc259c6058406f03ee36dc35d37a075
Max-Pare <resilientcrab@proton.me>
2026-01-08 15:51:54 +0100

    more stuffs

--- flask-server/app.py
+++ flask-server/app.py
@@ -7,6 +7,7 @@ app = Flask(
     #static_url_path='/site_files'
 )
 
+
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -15,11 +16,13 @@ def home():
 def error_page(msg:str="Errore interno."):
     return render_template('error.html', err_msg=msg)
 
+required_form_data = ['name', 'visitType', 'patientPhone']
+
 @app.route('/prenota', methods=['POST'])
 def prenota():
 
     def check_too_long(obj:dict):
-        return len(value) > 1024
+        return any(len(value) > 1024 for _, value in obj.items())
 
     def check_phone_valid(phone:str):
         '''Check if phone number is valid (no invalid characters and valid length)'''
@@ -29,20 +32,21 @@ def prenota():
         # We do this to make sure that if a bad actor sends a larger object than
         # expected we can just pull out the necessary data and discard the rest
         return {
-            'name'         : data.get('patientName'),
-            'visitType'    : data.get('visitType'),
-            'email'        : data.get('patientEmail'),
-            'patientPhone' : data.get('patientPhone'),
-            'patientNotes' : data.get('patientNotes')
-            }
-    return error_page('Errore !!!')
-    try:
-        data = request.form
-    except ValueError as e:
-        print('Received invalid/empty form data.')
-        return error_page('I dati inseriti non sono validi, riprovare.')
-    return extract_data(data)
-    return f"<h1>Richiesta ricevuta, verrete contattati al piu' presto.</h1><div></div><p>{data_obj}</p>"
+            'name'         : form_data.get('patientName'),
+            'visitType'    : form_data.get('visitType'),
+            'email'        : form_data.get('patientEmail'),
+            'patientPhone' : form_data.get('patientPhone'),
+            'patientNotes' : form_data.get('patientNotes')
+        }
+    cleaned_data = extract_data(request.form)
+    print(cleaned_data)
+    for k, v in cleaned_data.items():
+        if k in required_form_data and not v:
+            return error_page(f'Alcuno dati obbligatori non sono stati inseriti ({k})')
+    if check_too_long(cleaned_data):
+        return error_page('Errore: avete inserito troppi caratteri.')
+    if not check_phone_valid(cleaned_data.get('patientPhone')): return error_page('Il numero di telefono inserito non e\' valido.')
+    return f"<h1>Richiesta ricevuta, verrete contattati al piu' presto.</h1>"
 
 
 # if __name__ == "__main__":
