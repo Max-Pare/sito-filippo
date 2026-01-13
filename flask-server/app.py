cc721f6dafb64261bddda1154e865cdfc9d98795
Max-Pare <resilientcrab@proton.me>
2026-01-13 11:30:36 +0100

    minidb sqlite database

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,53 +1,99 @@
-from flask import Flask, render_template, request, abort
-import mysql.connector
+from flask import Flask, render_template, request
+import json
+import pathlib
+import os
+import minidb
+from datetime import datetime
+
+class Appointment(minidb.Model):
+    date_received = str
+    patient_name = str
+    visit_type = str
+    patient_email = str
+    patient_phone = str
+    patient_notes = str
+    
+DATA_DIR = './data'
+BOOKING_FILE = f'{DATA_DIR}/db.json' # may God have mercy on my soul
+db = minidb.Store('appointments.sqlite', debug=True)
+db.register(Appointment)
+required_form_data = ["name", "visitType", "patientPhone"]
+if not os.path.isdir(DATA_DIR):
+    os.mkdir(DATA_DIR)
 
 app = Flask(
     __name__,
-    #static_folder='site_files',
-    #static_url_path='/site_files'
+    # static_folder='site_files',
+    # static_url_path='/site_files'
 )
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route("/errore")
-def error_page(msg:str="Errore interno."):
-    return render_template('error.html', err_msg=msg)
 
-required_form_data = ['name', 'visitType', 'patientPhone']
+# @app.route("/errore")
+def result_page(msg: str = "Errore interno."):
+    return render_template("error.html", err_msg=msg)
 
-@app.route('/prenota', methods=['POST'])
+
+def appointify(form_data):
+    return Appointment(
+        patient_name = form_data.get("patientName"),
+        visit_type = form_data.get("visitType"),
+        patient_email = form_data.get("patientEmail"),
+        patient_phone = form_data.get("patientPhone"),
+        patient_notes = form_data.get("patientNotes"),
+        date_received = str(datetime.now())
+    )
+
+def log_critical():
+    pass
+
+def register_appointment(booking_data: dict):
+    appointment = appointify(booking_data)
+    appointment.save(db)
+    db.commit()
+    
+    
+    
+@app.route("/prenota", methods=["POST"])
 def prenota():
-
-    def check_too_long(obj:dict):
+    def check_too_long(obj: dict):
         return any(len(value) > 1024 for _, value in obj.items())
 
-    def check_phone_valid(phone:str):
-        '''Check if phone number is valid (no invalid characters and valid length)'''
-        return all(char in '0123456789+ ' for char in phone) and 10 <= len(phone) <= 15
-    
+    def check_phone_valid(phone: str):
+        """Check if phone number is valid (no invalid characters and valid length)"""
+        return 10 <= len(phone) <= 15
+        return all(char in "0123456789+ " for char in phone)
+
     def extract_data(form_data):
-        # We do this to make sure that if a bad actor sends a larger object than
-        # expected we can just pull out the necessary data and discard the rest
         return {
-            'name'         : form_data.get('patientName'),
-            'visitType'    : form_data.get('visitType'),
-            'email'        : form_data.get('patientEmail'),
-            'patientPhone' : form_data.get('patientPhone'),
-            'patientNotes' : form_data.get('patientNotes')
+            "patientName": form_data.get("patientName"),
+            "visitType": form_data.get("visitType"),
+            "email": form_data.get("patientEmail"),
+            "patientPhone": form_data.get("patientPhone"),
+            "patientNotes": form_data.get("patientNotes"),
         }
-    cleaned_data = extract_data(request.form)
-    print(cleaned_data)
-    for k, v in cleaned_data.items():
-        if k in required_form_data and not v:
-            return error_page(f'Alcuno dati obbligatori non sono stati inseriti ({k})')
-    if check_too_long(cleaned_data):
-        return error_page('Errore: avete inserito troppi caratteri.')
-    if not check_phone_valid(cleaned_data.get('patientPhone')): return error_page('Il numero di telefono inserito non e\' valido.')
-    return f"<h1>Richiesta ricevuta, verrete contattati al piu' presto.</h1>"
 
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port='38104', debug=True)
+    def check_data(form_data):
+        for k, v in form_data.items():
+            if k in required_form_data and not v:
+                return f"Alcuno dati obbligatori non sono stati inseriti ({k})"
+            if len(v) > 1024:
+                return "Avete inserito troppi caratteri."
+        if not check_phone_valid(form_data.get("patientPhone")):
+            return "Il numero di telefono inserito non e' valido."
+        return "OK"
+
+    form_data = extract_data(request.form)
+    if (_res := check_data(form_data)) != "OK":
+        return result_page(_res)
+    register_appointment(form_data)
+    for app in Appointment.load(db):
+        print(app)
+    return result_page(
+        "Richiesta ricevuta, verrete contattati al piu' presto."
+    )
